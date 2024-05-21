cce762bf7b1040109b237df68a5822cda83b088d
Franco <francopizzi2003@gmail.com>
2024-05-21 12:31:45 -0300

    primer commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);
+//EJERCICIO 1
+Console.WriteLine("Ingrese el numero a invertir");
+int num = int.Parse(Console.ReadLine());
+int reversed = 0;
+while (num > 0)
+{
+    int digit = num % 10;
+    reversed = reversed * 10 + digit;
+    num /= 10;
+}
+Console.WriteLine("El número invertido es: " + reversed);
+
